1a4e44531f62c3c0290d63a34c0c8aadf873bb96
Mykola <nazimkoff@gmail.com>
2020-10-15 18:58:12 +0300

    feat: done all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let map = new Map();
+    for (const argument of bracketsConfig) {
+        map.set(argument[1], argument[0]);
+    }
+
+    let stack = [];
+    for (const char of str) {
+        if (map.has(char)) {
+            if (char === map.get(char)) {
+                if (stack[stack.length - 1] === char) {
+                    stack.pop();
+                } else {
+                    stack.push(char);
+                }
+            } else {
+                if (stack.pop() !== map.get(char)) {
+                    return false
+                }
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.length === 0;
+};
